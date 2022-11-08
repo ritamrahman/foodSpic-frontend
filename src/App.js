f109a4c169e8f13e41c0e277e3d54c48db34c075
ritamrahman <ritamrahman@gmail.com>
2022-11-08 21:45:29 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import Coupon from "./Components/Coupon/Coupon";
 import CTA from "./Components/CTA/CTA";
+import Footer from "./Components/Footer/Footer";
 import Kitchens from "./Components/Kitchens/Kitchens";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="max-w-[1500px] mx-auto bg-primary pb-14">
+    <div className="max-w-[1500px] mx-auto bg-primary ">
       <Navbar />
       <Home />
       <Coupon />
       <Kitchens />
       <CTA />
+      <Footer />
     </div>
   );
 }
