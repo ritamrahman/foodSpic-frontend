086d483b5d3656ddca4bd03f9dcf9482fa3d3b8b
ritamrahman <ritamrahman@gmail.com>
2022-11-09 13:41:26 +0600

    kitchen details page added

---
+++ src/Components/Card/KitchenDetailsCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const KitchenDetailsCard = () => {
+  return (
+    <div className="px-4 py-16 my-14 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-1 bg-secondary lg:rounded-r-[50px] lg:rounded-rb-[50px] lg:rounded-lt-xl lg:rounded-lb-xl">
+      <div className="grid gap-5 row-gap-10 lg:grid-cols-2 ">
+        {/* details */}
+        <div className="flex flex-col justify-center items-center order-2">
+          <div className="max-w-xl mb-6">
+            <div>
+              {/* kitchen name */}
+              <h2 className="text-5xl font-Handlee text-center pb-10 text-primaryTextColor">Kitchen Name</h2>
+            </div>
+            {/* chef name */}
+            <div className="text-center">
+              <span className="text-2xl font-Handlee pr-3 font-extrabold ">Chef’s Name:</span>
+              <span className="text-2xl font-Handlee font-extrabold ">Ritam Rahman</span>
+            </div>
+            {/* stats */}
+            <div className="flex justify-around items-center my-11 font-Handlee text-center">
+              {/* complete order */}
+              <div>
+                <p>Total Completes Orders</p>
+                <span>300</span>
+              </div>
+              {/* reviews count */}
+              <div>
+                <p>Total Reviews</p>
+                <span>100</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* image */}
+        <div className="imageDiv max-h-96 ">
+          <img
+            className="object-cover w-full h-full py-5  lg:rounded-[70px] md:rounded-[70px] aspect-video sm:h-96 "
+            src="https://i.imagescrap.org/2022/11/08/Biryani-By-Kilo.webp"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default KitchenDetailsCard;
--- src/Pages/Kitchens/KitchenDetails.js
+++ src/Pages/Kitchens/KitchenDetails.js
@@ -1,7 +1,20 @@
 import React from "react";
+import KitchenDetailsCard from "../../Components/Card/KitchenDetailsCard";
 
 const KitchenDetails = () => {
-  return <div>KitchenDetails</div>;
+  return (
+    <div>
+      <KitchenDetailsCard />
+      <div className="container mx-20 py-12">
+        <h2 className="text-5xl font-Handlee text-center pb-10 text-primaryTextColor">Description</h2>
+        <article className="px-2 lg:px-36">
+          Description is the pattern of narrative development that aims to make vivid a place, object, character, or
+          group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In
+          practice it would be difficult to write literature that drew on just one of the four basic modes.
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default KitchenDetails;
