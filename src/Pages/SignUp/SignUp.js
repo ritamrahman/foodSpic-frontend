f9ce55617af61631b95e98bb5058040b2f660dcd
ritamrahman <ritamrahman@gmail.com>
2022-11-09 21:06:55 +0600

    fix signup issue

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import { GoogleAuthProvider } from "firebase/auth";
+import ErrorMsg from "../../Components/ErrorMsg/ErrorMsg";
 
 const SignUp = () => {
+  const [error, setError] = useState("");
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  console.log("location from signup", location);
+
+  // handleFormSubmit
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.photoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // createUser
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError("");
+        form.reset();
+        handleUpdateUserProfile(name, photoURL);
+        toast.success("Registration successfully");
+        user?.uid && navigate(from, { replace: true });
+      })
+      .catch((e) => {
+        console.error(e);
+        setError(e.message);
+      });
+  };
+
+  // handleUpdateUserProfile
+  const handleUpdateUserProfile = (name, photoURL) => {
+    const profile = {
+      displayName: name,
+      photoURL: photoURL,
+    };
+
+    updateUserProfile(profile)
+      .then(() => {})
+      .catch((error) => console.error(error));
+  };
+
+  const { providerLogin } = useContext(AuthContext);
+  const googleProvider = new GoogleAuthProvider();
+
+  // google signIn
+  const handleGoogleSignIn = () => {
+    providerLogin(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        user?.uid && toast.success("Login successful");
+        user?.uid && navigate(from, { replace: true });
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-primary py-10">
       {/* <!-- Login component --> */}
@@ -13,12 +79,16 @@ const SignUp = () => {
             <h1 className="text-xl font-semibold">Welcome back</h1>
             <small className="text-gray-400">Welcome back! Please enter your details</small>
 
+            <ErrorMsg error={error} />
+
             {/* <!-- Form --> */}
-            <form className="mt-4">
+            <form onSubmit={handleFormSubmit} className="mt-4">
               <div className="mb-3">
                 <label className="mb-2 block text-xs font-semibold">Full Name</label>
                 <input
                   type="text"
+                  id="fullName"
+                  name="fullName"
                   placeholder="Full Name"
                   className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
@@ -27,7 +97,9 @@ const SignUp = () => {
                 <label className="mb-2 block text-xs font-semibold">Avatar URL</label>
                 <input
                   type="text"
-                  placeholder="Avatar"
+                  placeholder="photoURL"
+                  id="photoURL"
+                  name="photoURL"
                   className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
               </div>
@@ -36,6 +108,8 @@ const SignUp = () => {
                 <input
                   type="email"
                   placeholder="Enter your email"
+                  id="email"
+                  name="email"
                   className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
               </div>
@@ -45,15 +119,23 @@ const SignUp = () => {
                 <input
                   type="password"
                   placeholder="*****"
+                  id="password"
+                  name="password"
                   className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
               </div>
 
               <div className="mb-3">
-                <button className="mb-1.5 block w-full text-center text-primary bg-primaryTextColor hover:bg-primary px-2 py-1.5 rounded-md">
+                <button
+                  className="mb-1.5 block w-full text-center text-primary bg-primaryTextColor hover:bg-primary px-2 py-1.5 rounded-md"
+                  type="submit"
+                >
                   Sign Up
                 </button>
-                <button className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
+                <button
+                  onClick={handleGoogleSignIn}
+                  className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md"
+                >
                   <img
                     className="w-5 mr-2"
                     src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
