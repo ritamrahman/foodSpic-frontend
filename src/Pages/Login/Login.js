dbd756fd70d109122e60ba917407543260f23057
ritamrahman <ritamrahman@gmail.com>
2022-11-09 12:19:15 +0600

    remove garbage

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,54 +3,55 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div class="flex flex-wrap min-h-screen w-full content-center justify-center bg-primary py-10">
+    <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-primary py-10">
       {/* <!-- Login component --> */}
-      <div class="flex shadow-md bg-secondary">
+      <div className="flex shadow-md bg-secondary">
         {/* <!-- Login form --> */}
-        <div class="w-[24rem] h-[32rem] flex flex-wrap content-center justify-center rounded-l-md bg-white">
-          <div class="w-72">
+        <div className="w-[24rem] h-[32rem] flex flex-wrap content-center justify-center rounded-l-md bg-white">
+          <div className="w-72">
             {/* <!-- Heading --> */}
-            <h1 class="text-xl font-semibold">Welcome back</h1>
-            <small class="text-gray-400">Welcome back! Please enter your details</small>
+            <h1 className="text-xl font-semibold">Welcome back</h1>
+            <small className="text-gray-400">Welcome back! Please enter your details</small>
 
             {/* <!-- Form --> */}
-            <form class="mt-4">
-              <div class="mb-3">
-                <label class="mb-2 block text-xs font-semibold">Email</label>
+            <form className="mt-4">
+              <div className="mb-3">
+                <label className="mb-2 block text-xs font-semibold">Email</label>
                 <input
                   type="email"
                   placeholder="Enter your email"
-                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                  className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
               </div>
 
-              <div class="mb-3">
-                <label class="mb-2 block text-xs font-semibold">Password</label>
+              <div className="mb-3">
+                <label className="mb-2 block text-xs font-semibold">Password</label>
                 <input
                   type="password"
                   placeholder="*****"
-                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                  className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
                 />
               </div>
 
-              <div class="mb-3 flex flex-wrap content-center">
-                <input id="remember" type="checkbox" class="mr-1 checked:bg-purple-700" />{" "}
-                <label for="remember" class="mr-auto text-xs font-semibold">
+              <div className="mb-3 flex flex-wrap content-center">
+                <input id="remember" type="checkbox" className="mr-1 checked:bg-purple-700" />{" "}
+                <label for="remember" className="mr-auto text-xs font-semibold">
                   Remember for 30 days
                 </label>
-                <a href="#" class="text-xs font-semibold text-purple-700">
+                <Link href="#" className="text-xs font-semibold text-purple-700">
                   Forgot password?
-                </a>
+                </Link>
               </div>
 
-              <div class="mb-3">
-                <button class="mb-1.5 block w-full text-center text-primary bg-primaryTextColor hover:bg-primary px-2 py-1.5 rounded-md">
+              <div className="mb-3">
+                <button className="mb-1.5 block w-full text-center text-primary bg-primaryTextColor hover:bg-primary px-2 py-1.5 rounded-md">
                   Sign in
                 </button>
-                <button class="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
+                <button className="flex flex-wrap justify-center w-full border border-gray-300 hover:border-gray-500 px-2 py-1.5 rounded-md">
                   <img
-                    class="w-5 mr-2"
+                    className="w-5 mr-2"
                     src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
+                    alt=""
                   />
                   Sign in with Google
                 </button>
@@ -58,9 +59,9 @@ const Login = () => {
             </form>
 
             {/* <!-- Footer --> */}
-            <div class="text-center">
-              <span class="text-xs text-gray-400 font-semibold">Don't have account?</span>
-              <Link to="/signup" class="text-xs font-semibold text-purple-700">
+            <div className="text-center">
+              <span className="text-xs text-gray-400 font-semibold">Don't have account?</span>
+              <Link to="/signup" className="text-xs font-semibold text-purple-700">
                 Sign up
               </Link>
             </div>
@@ -68,10 +69,11 @@ const Login = () => {
         </div>
 
         {/* <!-- Login banner --> */}
-        <div class="w-[24rem] h-[32rem] flex flex-wrap content-center justify-center rounded-r-md">
+        <div className="w-[24rem] h-[32rem] flex flex-wrap content-center justify-center rounded-r-md">
           <img
-            class="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md"
+            className="w-full h-full bg-center bg-no-repeat bg-cover rounded-r-md"
             src="https://i.imagescrap.org/2022/11/08/4622433-1.webp"
+            alt=""
           />
         </div>
       </div>
