01d901bf3c31ea6695eb981e2c2becb23e4cf5ef
ritamrahman <ritamrahman@gmail.com>
2022-11-10 21:35:04 +0600

    add favicon and notFound page

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { api } from "../../api/api";
+import NotFound from "../../Components/ErrorMsg/NotFound";
 import MetaData from "../../Components/Layout/MetaData";
 import { AuthContext } from "../../contexts/AuthProvider";
 
@@ -25,43 +26,47 @@ const MyReviews = () => {
   return (
     <>
       <MetaData title="My Reviews" />
-      <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
-        <h2 className="mb-4 text-2xl font-semibold leading-tight">My Reviews</h2>
-        <div className="overflow-x-auto">
-          <table className="min-w-full text-xs">
-            <colgroup>
-              <col />
-              <col />
-              <col />
-              <col className="w-24" />
-            </colgroup>
-            <thead className="dark:bg-gray-700">
-              <tr className="text-left text-secondary bg-secondaryBG">
-                <th className="p-3">Kitchen ID</th>
-                <th className="p-3">Review</th>
-                <th className="p-3">Date</th>
-                <th className="p-3">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {!loading &&
-                reviews?.map((review) => (
-                  <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
-                    <>
-                      <td className="p-3">{review.kitchenId}</td>
-                      <td className="p-3">{review.review}</td>
-                      <td className="p-3">{review.dateWhenCreated.split(":")[0]}</td>
-                      <td className="flex p-3 text-right">
-                        <button className="mx-2">Edit</button>
-                        <button className="mx-2">Delete</button>
-                      </td>
-                    </>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+      {reviews?.length === 0 ? (
+        <NotFound />
+      ) : (
+        <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+          <h2 className="mb-4 text-2xl font-semibold leading-tight">My Reviews</h2>
+          <div className="overflow-x-auto">
+            <table className="min-w-full text-xs">
+              <colgroup>
+                <col />
+                <col />
+                <col />
+                <col className="w-24" />
+              </colgroup>
+              <thead className="dark:bg-gray-700">
+                <tr className="text-left text-secondary bg-secondaryBG">
+                  <th className="p-3">Kitchen ID</th>
+                  <th className="p-3">Review</th>
+                  <th className="p-3">Date</th>
+                  <th className="p-3">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!loading &&
+                  reviews?.map((review) => (
+                    <tr className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                      <>
+                        <td className="p-3">{review.kitchenId}</td>
+                        <td className="p-3">{review.review}</td>
+                        <td className="p-3">{review.dateWhenCreated.split(":")[0]}</td>
+                        <td className="flex p-3 text-right">
+                          <button className="mx-2">Edit</button>
+                          <button className="mx-2">Delete</button>
+                        </td>
+                      </>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
