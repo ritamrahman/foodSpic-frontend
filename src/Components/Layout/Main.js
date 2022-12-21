e86b90fb36f2eb57872739ecc37c6d534953a5f2
ritamrahman <ritamrahman@gmail.com>
2022-12-22 01:59:26 +0600

    added spiner

--- src/Components/Layout/Main.js
+++ src/Components/Layout/Main.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import Loading from "../Loading/Loading";
 import Navbar from "../Navbar/Navbar";
 
 const Main = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(setIsLoading(false), 9000);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      {/* <div className="w-full pt-20 bg-white dark:bg-midnight"> */}
-      <Outlet />
-      {/* </div> */}
-      <Footer />
+      {isLoading === true ? (
+        <Loading />
+      ) : (
+        <>
+          <Navbar />
+          {/* <div className="w-full pt-20 bg-white dark:bg-midnight"> */}
+          <Outlet />
+          {/* </div> */}
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
